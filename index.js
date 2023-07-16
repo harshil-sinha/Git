28400f71adcbb31b08ca4e0918a1057670ffdf0f
harshil-sinha <harshilsinha17@gmail.com>
2023-07-16 17:51:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// var name = 'Yash Prasad';
+
+// var age = 25;
+
+// var address = "Prestige Falcon City, Bangalore";
+
+// console.log("personal details = My name is ",name, "age is", age," address is", address);
+
+
+// console.log("personal details = My name is " + name + "age is" + age + " address is" + address);
+
+
+// console.log(`$personal details = My name is ${name} age is ${age} address is ${address}`);
+// const ul = document.querySelector('.items');
+// ul.firstElementChild.innerHTML='HELLO';
+// ul.firstElementChild.style.color = 'green';
+// ul.children[1].style.color ='yellow';
+
+// const arr =[1,5,4,6,8];
+// let max = arr[0];
+// for(let i =1; i<arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+// var high = Number.MIN_VALUE;
+// var secondHigh = Number.MIN_VALUE;
+// for(var i=0; i<arr.length; i++){
+//     if(arr[i]>high){
+//         secondHigh = high;
+//         high = arr[i];
+//     }
+//     if(arr[i]<high && arr[i]>secondHigh){
+//         secondHigh = arr[i];
+//     }
+// }
+// console.log("Second highest is " + secondHigh);\
+
+
+// var temp = 308;
+// var temp2 = 35;
+// var unit = 'k';
+// if(unit==='k'){
+//     temp2 += 273;
+// }
+// if(unit==='c'){
+//     temp -= 273;
+// }
+// console.log(temp);
+
